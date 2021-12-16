9c2e04dfe87cbbeaea76aaf430e98ac74d0f4837
ttvtrendy <55133123+ttvtrendy@users.noreply.github.com>
2021-12-16 16:57:58 +0530

    Add files via upload

---
+++ C20-Student-boilerplate-code-main/sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var ground;
+
+var ball2;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  ball2 = Bodies.circle(300,20,10,ball_options);
+  World.add(world,ball);
+
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  ellipse(ball2.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,400,20);
+ 
+}
+
